6613fda85362fcb99856c34b1c64ad698983695f
Andy <agup@esri.com>
2022-04-13 16:24:57 -0600

    Initial test of performance script. (#99)
    * Initial test of performance script.
    
    * Tweak yaml
    
    * ci: analyze esm sample builds
    
    * A few more tweaks and another test run. Also bumped JS API version.
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- .github/scripts/analyze-builds.js
+++ .github/scripts/analyze-builds.js
@@ -2,41 +2,43 @@
 const { resolve } = require("path");
 const {
   createWriteStream,
-  promises: { readdir, readFile },
+  promises: { readdir, readFile }
 } = require("fs");
 const exec = require("util").promisify(require("child_process").exec);
-const { calculateBuildSize, logHeader } = require("./build-size.js");
+const { getBuildSizes } = require("./build-size.js");
+const browserPerformanceTest = require("./build-perf");
 
-const SAMPLES_PATH = resolve(__dirname, "../../esm-samples");
+const SAMPLES_PATH = resolve(__dirname, "..", "..", "esm-samples");
+const METRICS_PATH = resolve(SAMPLES_PATH, ".metrics");
 
 const SAMPLES_INFO = {
-  // "jsapi-angular-cli": {
-  //   name: "Angular",
-  //   package: "@angular/core",
-  //   buildPath: "dist" // relative path from the sample's root directory
-  // },
+  "jsapi-angular-cli": {
+    name: "Angular",
+    package: "@angular/core",
+    buildPath: "dist" // relative path from the sample's root directory
+  },
   "jsapi-create-react-app": {
     name: "CRA",
     package: "react-scripts",
-    buildPath: "build",
+    buildPath: "build"
   },
   "jsapi-vue": {
     name: "Vue",
     package: "vue",
-    buildPath: "dist",
+    buildPath: "dist"
   },
-  // "rollup": {
-  //   name: "Rollup",
-  //   package: "rollup",
-  //   devDep: true,
-  //   buildPath: "public"
-  // },
-  webpack: {
+  "rollup": {
+    name: "Rollup",
+    package: "rollup",
+    devDep: true,
+    buildPath: "public"
+  },
+  "webpack": {
     name: "Webpack",
     package: "webpack",
     devDep: true,
-    buildPath: "dist",
-  },
+    buildPath: "dist"
+  }
 };
 
 /**
@@ -48,6 +50,15 @@ const getDirectories = async (directoryPath) =>
   (await readdir(directoryPath, { withFileTypes: true }))
     .filter((dirent) => dirent.isDirectory() && dirent.name.charAt(0) !== ".")
     .map((dirent) => dirent.name);
+
+/**
+ * Emphasizes a message in the console
+ * @param {string} message - text to log
+ */
+const logHeader = (message) => {
+  const line = "-".repeat(message.length + 8);
+  console.log(`${line}\n|-> ${message} <-|\n${line}`);
+};
 
 /**
  * Executes a bash command, logs stderr, and returns stdout
@@ -64,53 +75,44 @@ const execLogErr = async (command) => {
   try {
     const sampleDirectories = await getDirectories(SAMPLES_PATH);
 
-    const jsapiVersions = new Set(
-      (
-        await Promise.all(
-          sampleDirectories.map(
-            async (sample) =>
-              !!SAMPLES_INFO[sample] &&
-              JSON.parse(
-                await readFile(
-                  resolve(__dirname, SAMPLES_PATH, sample, "package.json"),
-                  "utf8"
-                )
-              ).dependencies["@arcgis/core"].replace(/\^|\~/, "") // remove semver range
-          )
+    const jsapiVersions = (
+      await Promise.all(
+        sampleDirectories.map(
+          async (sample) =>
+            // only check versions of relevant samples
+            !!SAMPLES_INFO[sample] &&
+            JSON.parse(await readFile(resolve(__dirname, SAMPLES_PATH, sample, "package.json"), "utf8")).dependencies[
+              "@arcgis/core"
+            ].replace(/\^|\~/, "") // remove semver range
         )
-      ).filter((version) => !!version)
-    );
+      )
+    ).filter((version) => !!version);
 
-    if (jsapiVersions.size !== 1) {
-      console.log("ArcGIS JSAPI versions: ", jsapiVersions);
-      console.error(
-        "The samples have different versions of @arcgis/core, skipping build"
-      );
+    // unique ArcGIS JSAPI versions from the relevant samples
+    const jsapiVersionsUnique = new Set(jsapiVersions);
+
+    if (jsapiVersionsUnique.size !== 1) {
+      console.log("ArcGIS JSAPI versions: ", jsapiVersionsUnique);
+      console.error("The samples have different versions of @arcgis/core, skipping build");
       return;
     }
 
-    const [jsapiVersion] = jsapiVersions;
-    console.log(`ArcGIS JSAPI:  v${jsapiVersion}`);
-    const outputPath = resolve(
-      __dirname,
-      "../../esm-samples/.metrics",
-      `${jsapiVersion}.csv`
-    );
+    const [jsapiVersion] = jsapiVersionsUnique;
+    logHeader(`ArcGIS JSAPI:  v${jsapiVersion}`);
+    const outputPath = resolve(METRICS_PATH, `${jsapiVersion}.csv`);
     const stream = createWriteStream(outputPath);
     stream.write(
-      "Sample,Main bundle size (MB),On-disk size (MB), On-disk files\n"
+      "Sample,Build size (MB),Build file count,Main bundle file,Main bundle size (MB),Main bundle gzipped size (MB),Main bundle brotli compressed size (MB),Load time (ms), Total runtime (ms), Loaded size (MB), JS heap size (MB)\n"
     );
 
-    for (const sample of sampleDirectories) {
-      if (!SAMPLES_INFO[sample]) continue;
+    for (const [itemCount,sample] of sampleDirectories.entries()) {
+      if (!SAMPLES_INFO[sample]) continue; // skip samples with no info item
 
       const sampleName = SAMPLES_INFO[sample]?.name;
       const packageName = SAMPLES_INFO[sample]?.package;
       const samplePath = resolve(SAMPLES_PATH, sample);
-      const buildPath = SAMPLES_INFO[sample]?.buildPath;
-      const packageFile = JSON.parse(
-        await readFile(resolve(samplePath, "package.json"), "utf8")
-      );
+      const buildPath = resolve(samplePath, SAMPLES_INFO[sample]?.buildPath);
+      const packageFile = JSON.parse(await readFile(resolve(samplePath, "package.json"), "utf8"));
 
       const packageVersion = (
         !!SAMPLES_INFO[sample]?.devDep
@@ -119,24 +121,44 @@ const execLogErr = async (command) => {
       ).replace(/\^|\~/, "");
 
       logHeader(`${sampleName}: installing deps`);
-
-      const installOut = await execLogErr(`npm i --prefix ${samplePath}`);
+      const installOut = await execLogErr(`cd ${samplePath} && npm i`);
       console.log(installOut);
 
       logHeader(`${sampleName}: building`);
-      const buildOut = await execLogErr(`npm run build --prefix ${samplePath}`);
+      const buildOut = await execLogErr(`cd ${samplePath} && npm run build`);
       console.log(buildOut);
 
       logHeader(`${sampleName}: calculating build sizes`);
-      const { mainBundleSize, buildSize, buildFileCount } =
-        await calculateBuildSize({
-          samplePath,
-          buildPath,
-        });
+      const {
+        mainBundleName,
+        mainBundleSize,
+        mainBundleSizeGzip,
+        mainBundleSizeBrotli,
+        buildSize,
+        buildFileCount
+      } = await getBuildSizes(buildPath);
 
-      stream.write(
-        `${sampleName} ${packageVersion},${mainBundleSize},${buildSize},${buildFileCount}\n`
-      );
+      // convert bytes to megabytes
+      const mainBundleSizeMB = (mainBundleSize / 1024 ** 2).toFixed(2);
+      const mainBundleSizeGzipMB = (mainBundleSizeGzip / 1024 ** 2).toFixed(2);
+      const mainBundleSizeBrotliMB = (mainBundleSizeBrotli / 1024 ** 2).toFixed(2);
+      const buildSizeMB = (buildSize / 1024 ** 2).toFixed(2);
+
+      logHeader(`${sampleName}: calculating performance info`);   
+      // Creating a single object due to duplication of sampleName.
+      const perfResults = await browserPerformanceTest(buildPath, sampleName);
+      // convert bytes to megabytes
+      const pageTotalMB = (perfResults.pageTotalBytes / 1024 ** 2).toFixed(2);
+      const JSHeapUsedSizeMB = (perfResults.JSHeapUsedSizeBytes / 1024 ** 2).toFixed(2);
+
+      const output = `${sampleName} ${packageVersion},${buildSizeMB},${buildFileCount},${mainBundleName},${mainBundleSizeMB},${mainBundleSizeGzipMB},${mainBundleSizeBrotliMB},${perfResults.elapsedRuntimeMS},${perfResults.totalScriptTimeMS},${pageTotalMB},${JSHeapUsedSizeMB}\n`;
+
+      console.log("Writing results to CSV:", output);
+      stream.write(output);
+
+      if (itemCount === sampleDirectories.length - 1) {
+        console.log("\x1b[33m%s\x1b[0m", "JOB COMPLETE");
+      }      
     }
   } catch (err) {
     console.error(err);
