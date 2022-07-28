3d500e5cfcac681a68d7def8c794f05bf49d44a4
Ben Elan <benelan@users.noreply.github.com>
2022-07-28 16:32:37 -0700

    ci: only close past due milestones when they are completed

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,6 +1,25 @@
-const issueLabels = [{"id":4046962594,"node_id":"LA_kwDOF_Oe2s7xN7-i","url":"https://api.github.com/repos/benelan/test/labels/figma","name":"figma","color":"07D791","default":false,"description":""},{"id":4206213801,"node_id":"LA_kwDOF_Oe2s76tbqp","url":"https://api.github.com/repos/benelan/test/labels/0%20-%20new","name":"0 - new","color":"d4c5f9","default":false,"description":""},{"id":4206525500,"node_id":"LA_kwDOF_Oe2s76unw8","url":"https://api.github.com/repos/benelan/test/labels/milestone%20adjusted","name":"milestone adjusted","color":"e99695","default":false,"description":""}]
+const {
+  repo: { repo, owner },
+  payload: { issue, pull_request },
+} = context;
 
-const labels =  [...issueLabels
-.map((label) => label.name).filter(name => name !== "milestone adjusted"), "milestone adjusted"]
+const milestone_number = issue.milestone.id || pull_request.milestone.id;
 
-console.log(labels)
+const { data: milestone } = await github.issues.getMilestone({
+  owner,
+  repo,
+  milestone_number,
+});
+
+const currentDate = new Date(Date.now());
+currentDate.setUTCHours(0, 0, 0, 0);
+
+// close milestone if it is past due and there are no open issues/PRs
+!milestone.open_issues &&
+  Date(milestone.due_on) > currentDate &&
+  (await github.issues.updateMilestone({
+    owner,
+    repo,
+    milestone_number,
+    state: "closed",
+  }));
